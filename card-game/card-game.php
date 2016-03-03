5ca7110f5ba07d8e647bbb2e39ca645a5bcac637
Boverton <igrproductions@gmail.com>
2016-03-03 02:46:42 -0600

    Create card-game.php

---
+++ card-game/card-game.php
@@ -0,0 +1,61 @@
+<?php
+
+include('class_lib.php');
+
+?>
+
+<style>
+    #card {
+        margin: 10px;
+        height: 100px;
+        width: 75px;
+        border: solid 1px black;
+        text-align: center;
+        vertical-align: middle;
+        line-height: 100px;
+        display: inline-block;
+    }
+
+    #card_color {
+        color: red;
+    }
+</style>
+
+
+<?php
+
+// Create a deck and shuffle it
+$Deck = new Deck();
+$Deck->shuffle();
+
+// Create new players
+$player_bob = new Player('Bob');
+$player_sue = new Player('Sue');
+$player_brandon = new Player('Brandon');
+
+// Give bob 3 cards
+$player_bob->give_card($Deck->get_cards());
+$player_bob->give_card($Deck->get_cards());
+$player_bob->give_card($Deck->get_cards());
+
+// Give sue 3 cards
+$player_sue->give_card($Deck->get_cards());
+$player_sue->give_card($Deck->get_cards());
+$player_sue->give_card($Deck->get_cards());
+
+//give Brandon 3 cards
+$player_brandon->give_card($Deck->get_cards());
+$player_brandon->give_card($Deck->get_cards());
+$player_brandon->give_card($Deck->get_cards());
+
+// Show all the cards each player has been dealt
+echo '<h3>'.$player_bob->get_name().'</h3>';
+echo $player_bob->render();
+echo '<br/>';
+echo '<h3>'.$player_sue->get_name().'</h3>';
+echo $player_sue->render();
+echo '<br/>';
+echo '<h3>'.$player_brandon->get_name().'</h3>';
+echo $player_brandon->render();
+echo '<br/>';
+echo 'Number of cards remaining in the deck: '.$Deck->get_num_of_cards();
